b271a16e0bba6e92d5d365a615854ada5400508a
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-11-28 14:13:22 -0600

    add api

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM nodejs:alpine
+RUN mkdir /var/node
+WORKDIR /var/node
+ADD . /var/node
+RUN npm install
+EXPOSE 3000
+CMD npm start
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,64 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET status */
+router.get('/status', function(req, res, nex) {
+  res.send("I am healthy");
+});
+
+/* GET comicbooks */
+router.get('/comicbooks', function(req, res, nex) {
+  comicList = [
+    {
+      "title": "Action Comics",
+      "issue": 1005,
+      "publisher": "DC Comics",
+      "publish_date": "November 28, 2018",
+      "price": 3.99,
+      "writer": "Brian Michael Bendis",
+      "artist": "Ryan Sook",
+      "cover": "Ryan Sook",
+      "description": "The murderous mystery of the Red Cloud uncovered! Clark Kent draws closer to revealing a secret crime family that has operated for years in Metropolis, but the family's enforcer-the mysterious Red Cloud-proves she's a match for even the Man of Steel with an attack that leaves Superman breathless. Don't miss the last-page shocker as we reveal the true face of the Red Cloud!",
+      "page_count": 32,
+    },
+    {
+      "title": "Batman Beyond",
+      "issue": 26,
+      "publisher": "DC Comics",
+      "publish_date": "November 28, 2018",
+      "price": 3.99,
+      "writer": "Dan Jurgens",
+      "artist": ["Norm Rapmund", "Brett Booth"],
+      "cover": "Viktor Kalvachev",
+      "description": "The Joker’s back—and he wants to turn Batman and Robin into a punchline once and for all! The original Clown Prince of Crime returns to stalk Neo-Gotham in part two of “The Final Joke”! Can you guess what happens when he encounters Barbara Gordon for the first time in decades? And when Robin gets caught on a video feed for the first time, no one’s more delighted than The Joker.",
+      "page_count": 32,
+    },
+    {
+      "title": "Old Man Hawkeye",
+      "issue": 11,
+      "publisher": "Marvel Comics",
+      "publish_date": "November 28, 2018",
+      "price": 3.99,
+      "writer": "Ethan Sacks",
+      "artist": "Francesco Mobili",
+      "cover": "Marco Checchetto",
+      "description": "AT LAST - BARON ZEMO! WEAPON X - the facility that created Wolverine - now holds a shocking new secret. And finally, CLINT BARTON comes face-to-face with his ultimate target: BARON ZEMO, who masterminded the murder of the AVENGERS 45 years before! Don't miss the penultimate issue of OLD MAN HAWKEYE as Clint closes in on his quest and new revelations make you question what you thought you knew! Parental Advisory",
+      "page_count": 23,
+    },
+    {
+      "title": "Marvel Two-In-One ",
+      "issue": 12,
+      "publisher": "Marvel Comics",
+      "publish_date": "November 28, 2018",
+      "price": 3.99,
+      "writer": "Chip Zdarsky",
+      "artist": "Ramon Perez",
+      "cover": "Paul Renaud",
+      "description": "Our heroes were betrayed and left for dead, but now that the FANTASTIC FOUR are back, where’s RACHNA KOUL? The HUMAN TORCH and INVISIBLE WOMAN will soon find out, provided they can get past THE MOLE MAN!",
+      "page_count": 23,
+    }
+  ]
+  res.json(comicList);
+});
+
 module.exports = router;
